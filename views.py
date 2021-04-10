552815f79564861593396d027ac965de1f00cb11
kumkum98 <67735554+kumkum98@users.noreply.github.com>
2021-04-10 16:43:13 +0600

    Add files via upload
    hh

--- views.py
+++ views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
-from .models import Employee
 
-# Create your views here.
-def employee(request):
-    all_employee=Employee.objects.all()
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Seller
+from .forms import SellerInput
+
+from django.contrib.auth.decorators import login_required
+
+#Create your views here.
+@login_required
+def showseller(request):
+    alluser = Seller.objects.all()
+
+    contex = {
+        'userlist': alluser
+    }
+    return render(request, 'sellermanagement/sellerlist.html', contex)
+#@login_required
+
+def insertseller(request):
+    form=SellerInput()
+    msg="seller information "
+
+    if request.method=="POST":
+        form=SellerInput(request.POST)
+        #msg="not successful"
+        if form.is_valid():
+          form.save()
+          form=SellerInput()
+          msg="successfull🎈"
+          return redirect('insertpickup')
 
     context={
-        'employeelist':all_employee
+        'form':form,
+        'msg':msg
+
     }
 
-    return render(request,'Employee/employeeinfo.html',context)
+    return  render (request,'sellermanagement/insertseller.html',context)
+
+
 
