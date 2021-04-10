2a420030733c34578083fa9c0fbead921e5cbaa1
Rini <70469277+FarihaSultanaRini@users.noreply.github.com>
2021-04-10 16:44:35 +0600

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class SellerConfig(AppConfig):
-    name = 'Seller'
+class HomeConfig(AppConfig):
+    name = 'Home'
--- views.py
+++ views.py
@@ -1,58 +1,15 @@
-#from django.shortcuts import render,redirect
+
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm
-from .forms import ProfileForm
-from django.contrib.auth.decorators import login_required
-from .models import Profile
-# Create your views here.
-
-def registration(request):
-    form = UserCreationForm()
 
 
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
+def showhome(request):
 
-        if form.is_valid():
-         user=form.save(commit=False)
-         user.is_active=False
-         user.save()
-         return redirect('login')
-
-    context={
-        'form':form
-    }
-
-    return render(request,'Usermanagement/registration.html',context)
-@login_required
-def create_profile(request):
-    form = ProfileForm()
-
-    if request.method == "POST":
-        form = ProfileForm(request.POST, request.FILES)
-        if form.is_valid:
-            instance = form.save(commit=False)
-            instance.user = request.user
-            instance.save()
-            return redirect('view_profile')
-
-    context = {
-        'form' : form
-    }
-    return render(request, 'Usermanagement/createprofile.html', context)
-
-
-@login_required
-def view_profile(request):
-
-
-    profile=Profile.objects.get(user=request.user)
+    return  render(request,'home/home.html')
 
 
 
+def showpricelist(request):
 
-    context = {
-        'profile': profile
-    }
+    return  render(request,'home/pricelist.html')
 
-    return render(request, 'UserManagement/viewprofile.html', context)
+
